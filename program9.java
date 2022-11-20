e794424ee5f0c3f1e98f117e7b3dde27d7f29cb2
ar-un18 <bancroftarun@gmail.com>
2022-11-20 16:54:22 +0530

    added new file

---
+++ program9.java
@@ -0,0 +1,19 @@
+class program9
+{
+	public static void main (String[] args)
+	{
+		int age =36;
+		if (age<18)
+		{
+			System.out.println("minor");
+		}
+		else if (age>60)
+		{
+			System.out.println("senior");
+		}
+		else
+		{
+			System.out.println("major");
+		}
+	}
+}
